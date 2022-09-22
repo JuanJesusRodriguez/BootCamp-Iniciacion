350ee9630f8a6f04a852a6eaaee8aa240397f08c
Juan Jesús <108931438+JuanJesusRodriguez@users.noreply.github.com>
2022-09-23 00:19:47 +0200

    Parte 9

---
+++ Parte 9/03_ejercicio_crear_objeto/script.js
@@ -0,0 +1,8 @@
+let Coche = {
+    color :"Rojo",
+    marca : "Ford",
+    modelo: "Focud",
+    "cilindros y capacidad": "4 y 2.2",
+}
+
+console.log(Coche);
---
+++ Parte 9/07_ejercicio_añadir_atributos/script.js
@@ -0,0 +1,11 @@
+let perfil = {
+    nombre: "Juan Jesús",
+    edad: "24",
+};
+
+console.log(perfil.nombre);
+console.log(perfil.edad);
+
+perfil.altura = "1.83";
+
+console.log(perfil.altura);
